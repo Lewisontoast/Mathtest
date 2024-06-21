873d075e73cd5dc1511db858e2bf34572b7ffcc0
Lewisontoast <95551665+Lewisontoast@users.noreply.github.com>
2024-06-21 09:12:27 -0600

    Mathtest.py

--- Mathtest.py
+++ Mathtest.py
@@ -1,5 +1,5 @@
-import random #Best import ever on skib
-import math #Second best import ever on skib
+import random 
+import math 
 list1 = ['1+1=2?','2+2=4?','3+3=6?','4+4=8?','5+5=10?','15+15=30?','16+16=32?','17+17=34?','18+18=36?','19+19=38?']
 list2 = ['20+20=10?','6+6=2?','7+7=11?','8+8=32?','9+9=21?','10+10=0?','11+11=4?','12+12=12?','13+13=25?','14+14=13?']
 lst = list1 + list2
